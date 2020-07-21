2207c0638312b18362035d24068ebb541084b8c0
alex0735070005 <alex0735070005@gmail.com>
2020-07-21 05:47:35 +0300

    add generate bash script

--- app.sh
+++ app.sh
@@ -1,5 +1,4 @@
 ACTON="$1"
-NAME="$2"
 BASE_DIR=$(pwd)
 AVAILABLE_DIR=$BASE_DIR/docker/nginxServer/config/sites-available
 ENABLED_DIR=$BASE_DIR/docker/nginxServer/config/sites-enabled
@@ -7,18 +6,35 @@ HOSTS_DIR=$BASE_DIR/docker/nginxServer/config/hosts
 
 
 if [ "$ACTON" = "create-host" ]; then
+  read -p 'Enter host name: ' NAME
+  # copy from /nginxServer/config/host to nginxServer/config/sites-available/$NAME
   cp $HOSTS_DIR/example.conf $AVAILABLE_DIR/$NAME.conf
+
+  # rewrite {host_name} in nginxServer/config/sites-available/$NAME 
+  sed -i "s/{host_name}/$NAME/g" $AVAILABLE_DIR/$NAME.conf
+
+  # create link from /nginxServer/config/sites-enabled/$NAME to /nginxServer/config/sites-available/$NAME
   ln -s $AVAILABLE_DIR/$NAME.conf $ENABLED_DIR/$NAME.conf
-  mkdir $BASE_DIR/apps/$NAME
+
+  # create app dir if not exists /apps/$NAME
+  mkdir -p $BASE_DIR/apps/$NAME
+
+  #write host to /etc/hosts file
+  sudo sh -c "echo 127.0.0.1  $NAME >> /etc/hosts"
+
 elif [ "$ACTON" = "delete-host" ]; then
+  read -p 'Enter host name: ' NAME
+  # delete host files
   rm -f $AVAILABLE_DIR/$NAME.conf & rm -f $ENABLED_DIR/$NAME.conf
 elif ["$ACTON" = "run"]; then
+  # build and run docker container
   docker-compose up -d --build
 elif ["$ACTON" = "clear"]; then
+  # clear docker containers, images, volumes
   docker-compose down
   docker-compose down -v
   docker system prune -a --volumes -f
 else
-    echo 'Enter command: run | clear | create-host host_name'
+    echo 'Enter command: run | clear | create-host | delete-host'
     exit
 fi
